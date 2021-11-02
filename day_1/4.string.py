5cc06424d8a83ab2c9b57fd2ffba318942f2fd18
datory <sehwan622@gmail.com>
2021-11-02 19:27:48 +0900

    변수 수업 예제

--- day_1/4.string.py
+++ day_1/4.string.py
@@ -2,7 +2,11 @@
 print('Hello World')
 print("Hello WOrld")
 print("Hello \nWorld")
+
+# 따옴표 안에 따옴표를 인식할 수 있음
 print("Hello \"W\"orld")
+
+# 여러줄을 쉽게 작성할 수 있음
 print("""
 H
 E
---
+++ day_2/1_variable.py
@@ -0,0 +1,12 @@
+# built-in functions: 내장함수
+# len()
+# pow()
+# choice()
+# random()
+# 등등등
+
+
+가격: 1000
+부가가치세율: 0.1
+print(가격부가가치세율)
+
